ac646b89b08c032b4830b02179cbafd6ea214a56
Igloongn <igloongn@gmail.com>
2022-10-22 10:29:25 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { AuthProvider } from './context/AuthContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+
+      {/* <Container maxWidth="lg"> */}
+      {/* <Box sx={{ bgcolor: '#cfe8fc', height: '100%', pb: 5 }}> */}
+      <App />
+      {/* </Box>
+          </Container> */}
+    </AuthProvider>
+
+
   </React.StrictMode>
+
+
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
